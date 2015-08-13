85d5ecb21e1ceb39d662ee6e33e89d544103a693
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-08-13 12:53:00 +0900

    add plyaing video.

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port : 4000});
+  , wss = new WebSocketServer({ port : 4000})
+  , fs = require('fs');
 
 wss.on('connection', function connection(ws) {
 
   ws.on('message', function (message) {
     console.log('received:', message);
-    ws.send('received');
+    sendData(ws);
   })
 
   ws.on('close', function() {
     //TODO
   });
 });
+
+function sendData(ws) {
+  var path = __dirname + '/files/big-buck-bunny_trailer.webm';
+  fs.readFile(path, function(err, data) {
+    if (!err) {
+      ws.send(data);
+    }
+  });
+}
